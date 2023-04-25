99be0296e7b679d31d7b7bea1105bc8dd3d4f3fa
Narthana Epa <narthana.epa@gmail.com>
2023-04-25 12:53:19 +1000

    Make sure ALL tags match for acquired jobs

--- internal/controller/scheduler/scheduler.go
+++ internal/controller/scheduler/scheduler.go
@@ -3,12 +3,14 @@ package scheduler
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/url"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/buildkite/agent-stack-k8s/v2/internal/controller/agenttags"
 	"github.com/buildkite/agent-stack-k8s/v2/internal/controller/config"
 	"github.com/buildkite/agent-stack-k8s/v2/internal/controller/monitor"
 	"github.com/buildkite/agent-stack-k8s/v2/internal/version"
@@ -16,7 +18,7 @@ import (
 	"go.uber.org/zap"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
@@ -48,7 +50,10 @@ func New(logger *zap.Logger, client kubernetes.Interface, cfg Config) *worker {
 // NewInformerFactory returns an informer factory configured to watch resources (pods, jobs) created by the scheduler
 // matches pods labed with a job uuid and the agent tags that the scheduler was configured with
 func NewInformerFactory(k8s kubernetes.Interface, tags []string) (informers.SharedInformerFactory, error) {
-	labelsFromTags := TagsToLabels(nil, tags)
+	labelsFromTags, errs := agenttags.ToLabels(tags)
+	if len(errs) != 0 {
+		return nil, errors.Join(errs...)
+	}
 
 	requirements := make(labels.Requirements, 0, len(tags)+1)
 
@@ -103,7 +108,7 @@ func (w *worker) Create(ctx context.Context, job *monitor.Job) error {
 	}
 	_, err = w.client.BatchV1().Jobs(w.cfg.Namespace).Create(ctx, kjob, metav1.CreateOptions{})
 	if err != nil {
-		if errors.IsInvalid(err) {
+		if kerrors.IsInvalid(err) {
 			kjob, err = jobWrapper.BuildFailureJob(err)
 			if err != nil {
 				return fmt.Errorf("failed to create job: %w", err)
@@ -466,7 +471,11 @@ func (w *jobWrapper) BuildFailureJob(err error) (*batchv1.Job, error) {
 }
 
 func (w *jobWrapper) labelWithAgentTags() {
-	labels := TagsToLabels(w.logger, w.job.Tags)
+	labels, errs := agenttags.ToLabels(w.job.Tags)
+	if len(errs) != 0 {
+		w.logger.Warn("converting all tags to labels", zap.Errors("errs", errs))
+	}
+
 	for k, v := range labels {
 		w.k8sPlugin.Metadata.Labels[k] = v
 	}
